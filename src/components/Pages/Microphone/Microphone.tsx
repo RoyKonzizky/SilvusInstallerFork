832725af125a1d9bdcc356a65167a06150fec0ff
Hadar <hadar5445@gmail.com>
2024-03-24 14:47:22 +0200

    making the Microphone page a little shorter

--- src/components/Pages/Microphone/Microphone.tsx
+++ src/components/Pages/Microphone/Microphone.tsx
@@ -1,26 +1,13 @@
-import {ChangeEvent, useState} from "react";
+import {useState} from "react";
 
 function Microphone() {
     const [volume, setVolume] = useState(50);
-
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const newVolume = parseInt(event.target.value);
-        setVolume(newVolume);
-    };
 
     return (
         <div className="text-3xl h-screen flex flex-col justify-center items-center gap-y-8">
             <div className="flex gap-x-8 bg-[#303030]/70 p-3 rounded-xl">
                 <label>Volume</label>
-                <input
-                    type="range"
-                    id="volume"
-                    name="volume"
-                    min={0}
-                    max={100}
-                    value={volume}
-                    onChange={handleChange}
-                />
+                <input type="range" min={0} max={100} value={volume} onChange={(event) => setVolume(parseInt(event.target.value))}/>
             </div>
         </div>
     );
