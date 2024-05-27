2b2d9691b2bd9ce0cb82ce4d81d98fc2196cdec6
Roy Konzizky <roeicop110@gmail.com>
2024-05-28 02:47:01 +0300

    Clean up.

--- src/components/Pages/Topology/graph/ElementPopover.tsx
+++ src/components/Pages/Topology/graph/ElementPopover.tsx
@@ -10,20 +10,20 @@ interface IElementPopoverProps {
 export function ElementPopover(props: IElementPopoverProps) {
     return (
         <Popover title="Element Details" placement={"top"} arrow={false} open={!!props.selectedElement}
-            getPopupContainer={trigger => trigger.parentElement!}
-            overlayStyle={{
-                top: (!isNaN(props.position.y)) ? props.position.y : 100, left: props.position.x, height: '75px', position: 'absolute',
-                // transform: 'translate(-50%, -100%)'
-            }}
-            content={
-                <div>
-                    <p>{`This is the content of the popover for ${props.selectedElement?.id}`}</p>
-                    <button
-                        className={"bg-gray-900 w-9 h-9 focus:opacity-75 text-white font-bold py-2 px-4 rounded " +
-                            "focus:outline-none z-50 flex justify-center"} onClick={props.onClose}>X
-                    </button>
-                </div>
-            }
+                 getPopupContainer={trigger => trigger.parentElement!}
+                 overlayStyle={{top: (!isNaN(props.position.y)) ? props.position.y : 100, left: props.position.x,
+                     height: '75px', position: 'absolute',
+                     // transform: 'translate(-50%, -100%)'
+                 }}
+                 content={
+                     <div>
+                         <p>{`This is the content of the popover for ${props.selectedElement?.id}`}</p>
+                         <button
+                             className={"bg-gray-900 w-9 h-9 focus:opacity-75 text-white font-bold py-2 px-4 rounded " +
+                                 "focus:outline-none z-50 flex justify-center"} onClick={props.onClose}>X
+                         </button>
+                     </div>
+                 }
         />
     );
 }
