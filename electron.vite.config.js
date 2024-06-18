593a50ae002437274e7da29cf6cfeb76241ca941
Hadar <hadar5445@gmail.com>
2024-06-18 16:22:49 +0300

    changing it to electron type...

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,5 +1,5 @@
+import { defineConfig } from "electron-vite";
 import ReactRefresh from '@vitejs/plugin-react-refresh';
-import {defineConfig} from 'vite';
 
 export default defineConfig({
     plugins: [ReactRefresh()],
