7fb525fbf6588376fb9a7114389fd0977007112a
Hadar <hadar5445@gmail.com>
2024-06-20 09:40:36 +0300

    adding close button in presets in case of accidentally pressing on the preset button (and changed it to AppInputs in case of changing the buttons style)

--- src/renderer/components/PresetsButton/PresetsButtonModal/PresetsButtonModal.tsx
+++ src/renderer/components/PresetsButton/PresetsButtonModal/PresetsButtonModal.tsx
@@ -1,15 +1,24 @@
 import Modal from "react-modal";
 import {IPresetsButtonModalProps} from "./IPresetsButtonModalProps.ts";
 import {useState} from "react";
+import {Input} from "../../AppInputs/InputTypes/Input.ts";
+import {AppInputs} from "../../AppInputs/AppInputs.tsx";
 
 function PresetsButtonModal(props: IPresetsButtonModalProps) {
     const [selectedPreset, setSelectedPreset] = useState(props.selectedPreset);
     const presets = ["High", "Medium", "Low"];
 
-    const handleCloseModal = () => {
-        props.setSelectedPreset(selectedPreset);
-        props.setModalIsOpen(false);
-    };
+    const presetsInputs: Input[][] = [
+        [
+            {
+                type: "button", label: "Save", onClick: () => {
+                    props.setSelectedPreset(selectedPreset);
+                    props.setModalIsOpen(false);
+                }
+            },
+            {type: "button", label: "Close", onClick: () => props.setModalIsOpen(false)},
+        ]
+    ];
 
     return (
         <Modal isOpen={props.modalIsOpen} shouldCloseOnOverlayClick={false} ariaHideApp={false}
@@ -19,13 +28,13 @@ function PresetsButtonModal(props: IPresetsButtonModalProps) {
                 <div className="mb-2">Choose your Preset</div>
                 {presets.map((preset, index) => (
                     <div key={index} className="flex texl-3xl w-[400px]">
-                        <div className={`flex w-16 h-16 m-3 rounded-xl cursor-pointer ${selectedPreset === preset ? 'bg-[#303030]/70' : 'bg-white'}`}
-                             onClick={() => setSelectedPreset(preset)}/>
+                        <div
+                            className={`flex w-16 h-16 m-3 rounded-xl cursor-pointer ${selectedPreset === preset ? 'bg-[#303030]/70' : 'bg-white'}`}
+                            onClick={() => setSelectedPreset(preset)}/>
                         <div className="flex items-center">{preset}</div>
                     </div>
                 ))}
-                <input type="button" className="text-center mt-2 cursor-pointer bg-gray-800 inline-block px-6 py-3 font-semibold rounded-full border border-transparent border-3 border-teal-500 text-teal-500 transition duration-150 ease-in-out"
-                       onClick={handleCloseModal} value="Save"/>
+                <AppInputs appInputs={presetsInputs} className={'w-[200%]'}/>
             </div>
         </Modal>
     );
