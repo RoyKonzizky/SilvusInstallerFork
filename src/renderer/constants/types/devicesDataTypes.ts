3a437846c9e470e291b5b216545d3694df145ac7
Roy Konzizky <roeicop110@gmail.com>
2024-07-24 18:32:57 +0300

    Added camsType.

--- src/renderer/constants/types/devicesDataTypes.ts
+++ src/renderer/constants/types/devicesDataTypes.ts
@@ -1,3 +1,23 @@
 export type devicesType = { ip: string, id: string }[];
+
 export type batteriesType = { ip: string, percent: string }[];
+
 export type snrsType = { id1: string, id2: string, snr: string }[];
+
+export type camsType = {
+    message: string;
+    data: {
+        camera: {
+            ip: string;
+            connected_to: string;
+            main_stream: {
+                uri: string;
+                audio: number;
+            };
+            sub_stream: {
+                uri: string;
+                audio: number;
+            };
+        };
+    }[];
+};
