4afb390cb288e6768b79a376399fa24b21cff111
noammt <noamt@hatal.org.il>
2024-07-30 10:52:00 +0300

    add technical system button for support

--- src/renderer/components/Pages/Settings/Settings.tsx
+++ src/renderer/components/Pages/Settings/Settings.tsx
@@ -1,17 +1,16 @@
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from 'react-redux';
-import {Input} from "../../AppInputs/InputTypes/Input.ts";
-import {RootState} from "../../../redux/store.ts";
-import {ISettingsProps} from "./ISettingsProps.ts";
-import {updateTheSettingsState} from "../../../redux/Settings/settlingsSlice.ts";
-import {AppInputs} from "../../AppInputs/AppInputs.tsx";
-import {bandwidthValues, frequencyValues, totalTransitPowerValues} from "../../../constants/SilvusDropDownValues.ts";
-import {fetchBasicSettingsData, setBasicSettingsData} from "../../../utils/settingsUtils.ts";
-import PresetsButton from "../../PresetsButton/PresetsButton.tsx";
-import {changePreset} from "../../../utils/presetsUtils.ts";
-import {useTranslation} from 'react-i18next';
-// import {getInfoFromTheSilvusDevice} from "../../../scripts/getInfoFromTheSilvusDevice.ts";
-// import {SilvusDataType} from "../../../constants/SilvusDataType.ts";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { Input } from "../../AppInputs/InputTypes/Input.ts";
+import { RootState } from "../../../redux/store.ts";
+import { ISettingsProps } from "./ISettingsProps.ts";
+import { updateTheSettingsState } from "../../../redux/Settings/settlingsSlice.ts";
+import { AppInputs } from "../../AppInputs/AppInputs.tsx";
+import { bandwidthValues, frequencyValues, totalTransitPowerValues } from "../../../constants/SilvusDropDownValues.ts";
+import { fetchBasicSettingsData, setBasicSettingsData } from "../../../utils/settingsUtils.ts";
+import PresetsImage from "../../../assets/presets.svg";
+import { useTranslation } from 'react-i18next';
+import BottomCircle from "../../PresetsButton/BottomCircle/BottomCircle.tsx";
+import axios from "axios";
 
 export function Settings(props: ISettingsProps) {
     const [frequency, setFrequency] = useState(useSelector((state: RootState) => state.settings.frequency));
@@ -19,30 +18,30 @@ export function Settings(props: ISettingsProps) {
     const [networkId, setNetworkId] = useState(useSelector((state: RootState) => state.settings.networkId));
     const [totalTransitPower, setTotalTransitPower] = useState(useSelector((state: RootState) => state.settings.totalTransitPower));
     const ipAddress = useSelector((state: RootState) => state.ip.ip_address);
-    const [selectedPreset, setSelectedPreset] = useState(useSelector((state: RootState) => state.presets.chosenSpectrum));
+    
     const dispatch = useDispatch();
-    const {t, i18n} = useTranslation();
+    const { t, i18n } = useTranslation();
 
     const settingInputs: Input[][] = [
         [
-            {type: "text", label: t('frequency'), value: frequency, setValue: setFrequency, values: frequencyValues},
-            {type: "text", label: t('bandwidth'), value: bandwidth, setValue: setBandwidth, values: bandwidthValues}
+            { type: "text", label: t('frequency'), value: frequency, setValue: setFrequency, values: frequencyValues },
+            { type: "text", label: t('bandwidth'), value: bandwidth, setValue: setBandwidth, values: bandwidthValues }
         ], [
-            {type: "text", label: t('networkId'), value: networkId, setValue: setNetworkId},
-            {type: "text", label: t('totalTransitPower'), value: totalTransitPower, setValue: setTotalTransitPower, values: totalTransitPowerValues}
+            { type: "text", label: t('networkId'), value: networkId, setValue: setNetworkId },
+            { type: "text", label: t('totalTransitPower'), value: totalTransitPower, setValue: setTotalTransitPower, values: totalTransitPowerValues }
         ], [
             {
                 type: "button", label: t("save"),
                 onClick: async () => {
                     await setBasicSettingsData(false, parseFloat(frequency), bandwidth + " MHz", networkId, totalTransitPower);
-                    dispatch(updateTheSettingsState({frequency: frequency, bandwidth: bandwidth, networkId: networkId, totalTransitPower: totalTransitPower}))
+                    dispatch(updateTheSettingsState({ frequency: frequency, bandwidth: bandwidth, networkId: networkId, totalTransitPower: totalTransitPower }))
                 }
             },
             {
                 type: "button", label: t("saveNetwork"),
                 onClick: async () => {
                     await setBasicSettingsData(true, parseFloat(frequency), bandwidth + " MHz", networkId, totalTransitPower);
-                    dispatch(updateTheSettingsState({frequency: frequency, bandwidth: bandwidth, networkId: networkId, totalTransitPower: totalTransitPower}));
+                    dispatch(updateTheSettingsState({ frequency: frequency, bandwidth: bandwidth, networkId: networkId, totalTransitPower: totalTransitPower }));
                 }
             }
         ]
@@ -65,16 +64,29 @@ export function Settings(props: ISettingsProps) {
         loadData();
     }, [dispatch, ipAddress]);
 
-    useEffect(() => {
-        changePreset(selectedPreset, setFrequency, setBandwidth, setTotalTransitPower);
-    }, [selectedPreset]);
+    const openSilvusWebSystem = async () => {
+        try {
+            await axios.get('http://localhost:8080/silvus-tech-gui');
+        } catch (error) {
+            console.error('Error while opening Silvus Tech GUI in browser:', error);
+        }
+    }
 
     return (
         <>
             <div className={`${!props.isSmaller && "h-screen"} flex flex-col justify-center items-center gap-y-8`}>
                 <AppInputs appInputs={settingInputs} className={props.isSmaller ? 'flex-col' : `mr-[10%] ${i18n.language === 'en' ? 'w-[90%]' : 'w-[100%]'}`} isSmaller={props.isSmaller} />
             </div>
-            <PresetsButton selectedPreset={selectedPreset} setSelectedPreset={setSelectedPreset}/>
+            <div
+                onClick={openSilvusWebSystem}
+                className="flex items-start justify-start absolute bottom-6 left-6 cursor-pointer"
+            >
+                <BottomCircle
+                    image={PresetsImage}
+                    bgColor='#303030'
+                    radius={40}
+                />
+            </div>
         </>
     );
 }
