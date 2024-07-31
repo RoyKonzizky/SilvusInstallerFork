71be22b72ea46b4b37c60e52cd9dd94deb985449
Roy Konzizky <roeicop110@gmail.com>
2024-07-31 15:50:38 +0300

    Removed the initial group.

--- src/renderer/redux/TopologyGroups/initialTopologyGroupsState.ts
+++ src/renderer/redux/TopologyGroups/initialTopologyGroupsState.ts
@@ -1,9 +1,8 @@
 import { ITopologyGroupsReducerState } from "./ITopologyGroupsReducerState.ts";
 
-const initialHull = {id: "group1", members: []}
 
 export const initialTopologyGroupsState: ITopologyGroupsReducerState = {
     nodes: [],
     edges: [],
-    hullOptions: [initialHull],
+    hullOptions: [],
 };
