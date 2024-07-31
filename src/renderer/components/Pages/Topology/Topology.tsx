591b2092eccad8327b78848eb56877f67e546b00
Roy Konzizky <roeicop110@gmail.com>
2024-07-31 15:57:59 +0300

    Removed the cameras getter function.

--- src/renderer/components/Pages/Topology/Topology.tsx
+++ src/renderer/components/Pages/Topology/Topology.tsx
@@ -2,8 +2,8 @@ import {useEffect, useState} from 'react';
 import {ITopologyProps} from "./ITopologyProps";
 import {TopologyGraph} from "./graph/TopologyGraph";
 import {IUserEdge, IUserNode} from "@antv/graphin";
-import {createEdgesFromData, createNodesFromData, getCameras} from "../../../utils/topologyUtils/graphUtils.ts";
-import {batteriesType, camsType, devicesType, snrsType} from "../../../constants/types/devicesDataTypes.ts";
+import {createEdgesFromData, createNodesFromData} from "../../../utils/topologyUtils/graphUtils.ts";
+import {batteriesType, devicesType, snrsType} from "../../../constants/types/devicesDataTypes.ts";
 import useWebSocket from "react-use-websocket";
 import {useTranslation} from 'react-i18next';
 import {useDispatch, useSelector} from "react-redux";
@@ -17,7 +17,6 @@ export function Topology(props: ITopologyProps) {
         useState<batteriesType | null>(null);
     const [snrsData, setSnrsData] =
         useState<snrsType | null>(null);
-    const [cameras, setCameras] = useState<camsType | null>(null);
     const [graphData, setGraphData] =
         useState<{ nodes: IUserNode[], edges: IUserEdge[] } | null>(null);
     const ws_url = "ws://localhost:8080/ws";
@@ -56,7 +55,7 @@ export function Topology(props: ITopologyProps) {
     useEffect(() => {
         if (devices && batteries && snrsData) {
             try {
-                const nodes = createNodesFromData(devices, batteries, cameras!);
+                const nodes = createNodesFromData(devices, batteries);
                 const edges = createEdgesFromData(snrsData);
 
                 const updatedNodes = nodes.map(newNode => {
@@ -85,18 +84,6 @@ export function Topology(props: ITopologyProps) {
         }
     }, [devices, batteries, snrsData]);
 
-    useEffect(() => {
-        const loadCamerasData = async () => {
-            try {
-                const camerasData = await getCameras();
-                setCameras(camerasData);
-            } catch (error) {
-                console.error("Error in receiving cameras", error);
-            }
-        };
-
-        loadCamerasData();
-    }, [devices]);
 
     return (
         <div className={`${props.isSmaller ? 'w-[35%] h-[80%]' : 
