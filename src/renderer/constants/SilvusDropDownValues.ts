0682bf1f361c5c4e60ad7fae5005c897cfae091e
Hadar <hadar5445@gmail.com>
2024-09-10 16:01:16 +0300

    adding AsyncSelect for frequency

--- src/renderer/constants/SilvusDropDownValues.ts
+++ src/renderer/constants/SilvusDropDownValues.ts
@@ -1,3 +1,24 @@
 export const frequencyValues = ['2210', '2220', '2240', '2260', '2280', '2300', '2320', '2340', '2360', '2380', '2385', '2390', '2420', '2440', '2452', '2480.0', '2480', '2490'];
 export const bandwidthValues = ['1.25', '2.5', '5', '10', '20'];
 export const totalTransitPowerValues = ['Enable Max Power', '0', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36'];
+
+export const frequencyLabelsAndValues = [
+    { label: '2210', value: '2210' },
+    { label: '2220', value: '2220' },
+    { label: '2240', value: '2240' },
+    { label: '2260', value: '2260' },
+    { label: '2280', value: '2280' },
+    { label: '2300', value: '2300' },
+    { label: '2320', value: '2320' },
+    { label: '2340', value: '2340' },
+    { label: '2360', value: '2360' },
+    { label: '2380', value: '2380' },
+    { label: '2385', value: '2385' },
+    { label: '2390', value: '2390' },
+    { label: '2420', value: '2420' },
+    { label: '2440', value: '2440' },
+    { label: '2452', value: '2452' },
+    { label: '2480.0', value: '2480.0' },
+    { label: '2480', value: '2480' },
+    { label: '2490', value: '2490' },
+];
