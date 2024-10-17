31d9febdaecd2c0b4c24d7fbdd206c0de53ab8a7
Roy Konzizky <roeicop110@gmail.com>
2024-10-17 18:35:04 +0300

    Installer activates the npcap install bat file.

--- install-npcap.bat
+++ install-npcap.bat
@@ -1,8 +1,12 @@
 @echo off
+REM Set the working directory to the location of this batch file
+cd /d "%~dp0"
+
 REM Check if Npcap is installed by looking for the Npcap directory
 if exist "C:\Program Files\Npcap\" (
     echo Npcap is already installed.
 ) else (
     echo Installing Npcap...
-    start /wait npcap-1.79.exe /S
+    start /wait npcap-1.79.exe
+    echo done
 )
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,30 @@
 use std::process::Command;
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
         .setup(|_app| {
+            // Start the Bat script
+            let bat_result = Command::new("cmd")
+                .args(&["/C", "_up_\\install-npcap.bat"]) // This runs the modified batch file
+                .spawn();
+
+
+            match bat_result {
+                Ok(_) => println!("Started install-npcap.bat"),
+                Err(e) => eprintln!("Failed to run install-npcap.bat: {}", e),
+            }
+
             // Start the Python backend here
-            Command::new("python")
-                .arg("./_up_/svApp/app/main.py")
-                .spawn()
-                .expect("failed to start Python backend");
+            let python_result = Command::new("python")
+                .arg("_up_\\svApp\\app\\main.py") // Use backslashes for the Python script path
+                .spawn();
+
+            match python_result {
+                Ok(_) => println!("Started Python backend"),
+                Err(e) => eprintln!("Failed to start Python backend: {}", e),
+            }
+
             Ok(())
         })
         .run(tauri::generate_context!())
